5d0d08839974fbebd0c41a42da64900a06e9e0e7
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-08-18 18:34:01 +0930

    Update files

--- server.js
+++ server.js
@@ -12,18 +12,13 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('*', (req, res) =>
+app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
